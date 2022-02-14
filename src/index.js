6f70a32195234536060918f2f46ae1f9d48234d7
Adam L <luzsiadam@gmail.com>
2022-02-14 18:15:27 +0100

    + Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import { registerBlockType } from "@wordpress/blocks";
-import { useBlockProps } from '@wordpress/block-editor';
+import { registerBlockType } from '@wordpress/blocks';
+
+import Edit from './edit.jsx';
+import Save from './save.jsx';
 
 import './style.scss';
-import './style-editor.scss';
 
-registerBlockType("blocks-course/firstblock", {
-    edit: function () {
-        const blockProps = useBlockProps();
-        //console.log(blockProps);
-        return <p {...blockProps}>Edit JSXA</p>;
-    },
-    save: function () {
-        const blockProps = useBlockProps.save();
-        return <p {...blockProps}>Save JSX</p>;
-    },
+registerBlockType('blocks-course/firstblock', {
+	edit: Edit,
+	save: Save,
 });
